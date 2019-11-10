bff5ba6b50c97ca7852933ab48f676c9a435aa95
Maddie <maddiemcampbell@gmail.com>
2019-11-10 08:45:50 -0800

    Revert "inaturalist people data is populating"
    This reverts commit e95d02a7bb4491a4c5a3cb74523e070cf70443f5.

--- server/server.js
+++ server/server.js
@@ -7,27 +7,9 @@ const urlEncodedParser = bodyParser.urlencoded({extended: false});
 const app = express();
 dotenv.config();
 
+app.use(express.static('dist'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(urlEncodedParser);
-
-const inaturalistUrl = 'https://api.inaturalist.org/v1/';
-
-app.get('/observations', (req, res) => {
-    const taxonId = req.query;
-    const testTest = 'taxon_id=43693&captive=false&mappable=true&threatened=true&d1=2019-01-01&d2=2019-01-01';
-
-    // https://api.inaturalist.org/v1/observations?taxon_id=43693&captive=false&mappable=true&threatened=true&d1=2019-01-01&d2=2019-11-09
-
-    axios.get(`${inaturalistUrl}observations?${testTest}`)
-        .then((result) => {
-            res.send(result.data);
-            console.log('RESULT: ', result.data.results[0].observed_on);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.send('An error occured.');
-        })
-});
 
 module.exports = app;
