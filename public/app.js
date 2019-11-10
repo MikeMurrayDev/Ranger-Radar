479bf6b6eec3942f0702f5d2b37f7a387d554c3b
Mike Murray <MikeMurrayDev@gmail.com>
2019-11-09 23:55:19 -0800

    removes dotenv package; removes extra and commented out code

--- public/app.js
+++ public/app.js
@@ -1,63 +1,16 @@
 function handleAnimalSubmit(event) {
-  // var animalId = document.getElementById('animal_dropdown').selectedIndex;
-  // var animalValue = document.getElementsByTagName("option")[animalId].value;
-  // console.log('Animal Value: ', animalValue);
-
-  // var captive = document.getElementById('captive_dropdown').nodeValue;
-  // var captiveValue = document.getElementsByTagName("option")[captive].value;
-  // console.log('captiveValue: ', captiveValue);
-
-  // var mappable = document.getElementById('mappable_dropdown').nodeValue;
-  // var mappableValue = document.getElementsByTagName("option")[mappable].value;
-  // console.log('mappableValue: ', mappableValue);
-
-  // var threatened = document.getElementById('threatened_dropdown').nodeValue;
-  // var threatenedValue = document.getElementsByTagName("option")[threatened].value;
-  // console.log('threatenedValue: ', threatenedValue);
-
-  // var startDate = document.getElementById('start_date').value;
-  // var startDateValue = document.getElementsByTagName("option")[startDate].value;
-  // console.log('startDateValue: ', startDateValue);
-
-  // var endDate = document.getElementById('end_date').value;
-  // var endDateValue = document.getElementsByTagName("option")[endDate].value;
-  // console.log('endDateValue: ', endDateValue);
   
   event.preventDefault();
 
-  // var URLBase = "https://api.inaturalist.org/v1/observation?";
-  
-  // var finalURL = URLBase + 'taxon_id=' + animalValue + '&captive=' + captiveValue + '&mappable=' + mappableValue + '&threatened=' + 
-  //   threatenedValue + '&d1=' + startDateValue + '&d2=' + endDateValue;
-
-  // console.log('FINAL URL: ', finalURL);
-  
-  let fetchData = {
-    method: 'GET',
-    mode: 'no-cors',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }
-
   fetch('https://api.inaturalist.org/v1/observations?taxon_id=43693&captive=false&mappable=true&threatened=true&d1=2019-01-01&d2=2019-11-09')
-  .then(res => res.json())
-  .then(data => {
-    var locations = data.results.map(el => el.location);
-    // console.log('HEYO: ', locations);
+    .then(res => res.json())
+    .then(data => {
+      var locations = data.results.map(el => el.location);
+      // console.log('HEYO: ', locations);
 
-    var observed = data.results.map(el => el.observed_on);
-    // console.log('YOHE: ', observed);
-  });
+      var observed = data.results.map(el => el.observed_on);
+      // console.log('YOHE: ', observed);
+    });
 
-  fetch('http://rangerradar.pythonanywhere.com/sightings', fetchData)
-  .then(res => res.json())
-  .then(console.log('hi: ', res.json))
-  .then(data => {
-    var locations = data.map(el => el.lat);
-    console.log('HEYO: ', locations);
-
-    var observed = data.map(el => el.long);
-    console.log('YOHE: ', observed);
-  });
+  
 }
--- server/server.js
+++ server/server.js
@@ -1,33 +1,24 @@
 const express = require('express');
 const axios = require('axios');
-const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 const app = express();
-dotenv.config();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(urlEncodedParser);
 
-const inaturalistUrl = 'https://api.inaturalist.org/v1/';
-
-app.get('/observations', (req, res) => {
-    const taxonId = req.query;
-    const testTest = 'taxon_id=43693&captive=false&mappable=true&threatened=true&d1=2019-01-01&d2=2019-01-01';
-
-    // https://api.inaturalist.org/v1/observations?taxon_id=43693&captive=false&mappable=true&threatened=true&d1=2019-01-01&d2=2019-11-09
-
-    axios.get(`${inaturalistUrl}observations?${testTest}`)
-        .then((result) => {
-            res.send(result.data);
-            console.log('RESULT: ', result.data.results[0].observed_on);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.send('An error occured.');
-        })
+app.get('/getit', (req, res) => {
+    axios.get('http://rangerradar.pythonanywhere.com/sightings')
+    .then((result) => {
+        res.send(result.data);
+        console.log('RESULT SUCKAH: ', result.data);
+    })
+    .catch((error) => {
+        console.error(error);
+        res.send('An error occured.');
+    })
 });
 
 module.exports = app;
